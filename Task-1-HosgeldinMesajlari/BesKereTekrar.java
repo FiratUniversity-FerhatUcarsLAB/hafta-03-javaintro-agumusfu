652a8e7d36d285835f968e1c6534a5252ae52fe3
agumusfu <250542016@firat.edu.tr>
2025-10-23 21:05:24 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class BesKereTekrar{
+        public static void main(String[] arcg){
+            System.out.println("Java'ya Hoş Geldiniz");
+            System.out.println("Java'ya Hoş Geldiniz");
+            System.out.println("Java'ya Hoş Geldiniz");
+            System.out.println("Java'ya Hoş Geldiniz");
+            System.out.println("Java'ya Hoş Geldiniz");
+        }
+}
